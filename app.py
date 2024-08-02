c64abfbdc44de461ec5e843ab02e6653bc00f1db
Soumyajit Kolay <skolay@vivaldi.net>
2024-08-02 23:17:59 +0530

    Added Flask app

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, url_for, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+loaded_model = pickle.load(open("model.pkl", "rb"))
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+def WeightPredictor(height):
+    result = loaded_model.predict([[height]])
+    return "Predicted Weight is " + str(result[0][0])
+
+def WeightPredictor_value(height):
+    result = loaded_model.predict([[height]])
+    return str(result[0][0])
+
+@app.route("/results", methods=["POST"])
+def output():
+    if request.method == "POST":
+        h = int(request.form['h'])
+        return render_template("result.html", height=h, weight=WeightPredictor_value(h))
+        # return WeightPredictor(h)
+
+if __name__ == "__main__":
+    app.run()
