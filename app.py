2dbbd7f3c8327b6e467eac5a7cc6fd24ab79a33f
Soumyajit Kolay <skolay@vivaldi.net>
2024-08-03 17:45:34 +0530

    Enabled HTML Rendered Output back again, minor code refactor

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, redirect, url_for, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 import pickle
-import numpy as np
+# import numpy as np
 
 app = Flask(__name__)
 loaded_model = pickle.load(open("model.pkl", "rb"))
@@ -9,28 +9,26 @@ loaded_model = pickle.load(open("model.pkl", "rb"))
 def index():
     return render_template("index.html")
 
-def WeightPredictor(height):
-    result = loaded_model.predict([[height]])
-    return "Predicted Weight is " + str(result[0][0]) + "kg"
+# def WeightPredictor(height):
+#     result = loaded_model.predict([[height]])
+#     return "Predicted Weight is " + str(result[0][0]) + "kg"
 
 # Uncomment Lines To use HTML 
 
-# def WeightPredictor_value(height):
-#     result = loaded_model.predict([[height]])
-#     return str(result[0][0])
+def WeightPredictor_value(height):
+    result = loaded_model.predict([[height]])
+    return str(result[0][0])
 
 @app.route("/results", methods=["POST"])
 def output():
-    if request.method == "POST":
-        h = int(request.form['h'])
-        # return render_template("result.html", height=h, weight=WeightPredictor_value(h))
-        return WeightPredictor(h) # COMMENT THIS IF THE ABOVE LINE IS NOT COMMENTED
+    h = int(request.form['h'])
+    return render_template("result.html", height=h, weight=WeightPredictor_value(h))
+    # return WeightPredictor(h) # COMMENT THIS IF THE ABOVE LINE IS NOT COMMENTED
 
 # REST API
 @app.route("/predict", methods=["POST"])
 def predict():
     json_ = request.json
-    data = json_[0]
     output = []
     for i in range(len(json_)):
         prediction = loaded_model.predict([[json_[i]["height"]]])
